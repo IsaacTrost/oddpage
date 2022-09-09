da5d2699dffed818acaf5be284e6d8df0c36642f
Isaac Trost <wisaactrost@gmail.com>
2022-09-08 22:53:25 -0400

    made odd game

--- src/index.js
+++ src/index.js
@@ -3,16 +3,63 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class App extends React.Component{
-  
+  constructor(props){
+    super(props);
+    this.state = {
+      clickcounter: 0,
+      hovercounter: 0,
+      offsetx: 0,
+      offsety: 0,
+      timerstarted: 0,
+    }
+  }
+  clicky = function(){
+    console.log(5);
+    this.setState({
+      clickcounter: this.state.clickcounter+1,
+    })
+  }
+  movey = function(){
+    
+    setTimeout(() => {
+    this.setState({
+      offsetx: Math.random()*500,
+      offsety: Math.random()*500,
+      hovercounter: this.state.hovercounter+1,
+      timerstarted: 1,
+    })}, 10000)
+  }
   render(){
-    var counter=0;
+    
     return (
       
       <div className = "App">
-        <h1>Hola, youve clicked {counter} times</h1>
+        <div>
+          <h4>Hola, you have clicked {this.state.clickcounter} times <br/>
+          you have hovered {this.state.hovercounter} times</h4>
+        </div>
+        <div>
+          <Button
+          onClick={() => this.clicky()}
+          onMove={() => this.movey()}
+          offsetx = {this.state.offsetx}
+          offsety = {this.state.offsety} />
+        </div>
       </div>
     )
   }
+}
+function Button(props){
+  return (
+    
+    <button className = "button" 
+    onClick={props.onClick} 
+    onMouseMove = {props.onMove}
+    style = {{marginLeft: props.offsetx, marginTop: props.offsety, height: 100}}
+    >
+      "click me lol"
+    </button>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
