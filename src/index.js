35b23b732eb68d3297e3c3cc7b3805c7c424dc19
Isaac Trost <wisaactrost@gmail.com>
2022-09-15 15:41:14 -0400

    fixed moving logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class App extends React.Component{
+  
   constructor(props){
     super(props);
     this.state = {
@@ -11,7 +12,9 @@ class App extends React.Component{
       offsetx: 0,
       offsety: 0,
       timerstarted: 0,
+      moving: false,
     }
+
   }
   clicky = function(){
     console.log(5);
@@ -20,14 +23,21 @@ class App extends React.Component{
     })
   }
   movey = function(){
+    if (!this.state.moving){
+      this.setState({
+        moving: true,
+      })
+      setTimeout(() => {
+        this.setState({
+          offsetx: Math.random()*500,
+          offsety: Math.random()*500,
+          hovercounter: this.state.hovercounter+1,
+          timerstarted: 1,
+          moving: false,
+        })
+      }, 200)
     
-    setTimeout(() => {
-    this.setState({
-      offsetx: Math.random()*500,
-      offsety: Math.random()*500,
-      hovercounter: this.state.hovercounter+1,
-      timerstarted: 1,
-    })}, 10000)
+  }
   }
   render(){
     
@@ -53,7 +63,7 @@ function Button(props){
   return (
     
     <button className = "button" 
-    onClick={props.onClick} 
+    onMouseDown={props.onClick} 
     onMouseMove = {props.onMove}
     style = {{marginLeft: props.offsetx, marginTop: props.offsety, height: 100}}
     >
@@ -62,5 +72,33 @@ function Button(props){
   )
 }
 
+function Timer(props){
+  const [timer, setTimer] = React.useState('00:00:00')
+  const getTimeRemaining = (e) => {
+    const total = Date.parse(e) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / 1000 / 60 / 60) % 24);
+    return {
+        total, hours, minutes, seconds
+    };
+}
+const startTimer = (e) => {
+  let { total, hours, minutes, seconds } 
+              = getTimeRemaining(e);
+  if (total >= 0) {
+
+      // update the timer
+      // check if less than 10 then we need to 
+      // add '0' at the beginning of the variable
+      setTimer(
+          (hours > 9 ? hours : '0' + hours) + ':' +
+          (minutes > 9 ? minutes : '0' + minutes) + ':'
+          + (seconds > 9 ? seconds : '0' + seconds)
+      )
+  }
+}
+
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
